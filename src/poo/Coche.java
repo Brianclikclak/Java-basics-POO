711375f4d105ac3092a002ef3948e59cdb72854f
Brianclikclak <brianclikclak@hotmail.com>
2023-08-03 17:32:38 +0200

    adding new methods

--- src/poo/Coche.java
+++ src/poo/Coche.java
@@ -9,12 +9,11 @@ public class Coche {
     private int motor;
     private int peso_plataforma;
     String color;
-
     int peso_total;
     boolean asientos_cuero, climatizador;
 
-    public Coche(){ // Este el metodo constructor
 
+    public Coche(){ // Este el metodo constructor
         ruedas = 4;
         largo = 2000;
         ancho = 300;
@@ -22,20 +21,39 @@ public class Coche {
         peso_plataforma = 500;
     }
 
-    public String getlargo(){  // Este es el metodo GETTER
-
-        return "El largo del coche es " + largo + "cm";
+    public String dime_datos_generales(){  // Este es el metodo GETTER
+        return "La plataforma del vehiculo tiene " + ruedas + " ruedas." + " Mide " + largo/1000 +
+                " metros con un ancho de " + ancho + " cm y un peso de plataforma de " + peso_plataforma + " kg.";
     }
 
-    public void setcolor(){  // Este es el metodo SETTER - Establecemos el color del coche
-
-        color = "azul";
+    public void set_color(String color_coche){  // Este es el metodo SETTER - Establecemos el color del coche
+        color = color_coche;
     }
 
-    public String getcolor(){ // Este es el metodo GETTER - Retornamos el color del coche
-
+    public String get_color(){ // Este es el metodo GETTER - Retornamos el color del coche
         return "El color del coche es " + color;
     }
+
+    public void configura_asientos(String asientos_cuero){  //Metodo SETTER
+
+        if(asientos_cuero == "si"){
+            this.asientos_cuero = true;
+        }
+        else{
+            this.asientos_cuero = false;
+        }
+    }
+
+    public  String dime_asientos(){  // Metodo GETTER
+
+        if(asientos_cuero){
+            return "El coche tiene asientos de cuero";
+        }
+        else{
+          return "El coche tiene asientos de serie";
+        }
+    }
+
 
 
 
