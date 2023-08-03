849290817e13214ab8359c95abfdfcb96546386b
Brianclikclak <brianclikclak@hotmail.com>
2023-08-03 19:40:03 +0200

    checking price method

--- src/poo/Uso_Coche.java
+++ src/poo/Uso_Coche.java
@@ -1,19 +1,28 @@
 package poo;
 
+import javax.swing.*;
+
 public class Uso_Coche {
 
     public static void main(String[] args) {
 
         Coche Seat = new Coche();
-        Seat.set_color("amarillo");
+
+        Seat.set_color(JOptionPane.showInputDialog("Introduce de que color desea el coche"));
         System.out.println(Seat.get_color());
+
         System.out.println(Seat.dime_datos_generales());
-        Seat.configura_asientos("no");
+
+        Seat.configura_asientos(JOptionPane.showInputDialog("¿Desea asientos de cuero incorporados?"));
         System.out.println(Seat.dime_asientos());
-        Seat.configura_climatizador("si");
+
+        Seat.configura_climatizador(JOptionPane.showInputDialog("¿Desea climatizador incorporado?"));
         System.out.println(Seat.dime_climatizador());
-        Seat.dime_peso_coche();
-        System.out.println(Seat.peso_total);
+
+
+        System.out.println(Seat.dime_peso_coche());
+
+        System.out.println("El precio final del coche es: " + Seat.precio_coche() + "€.");
 
     }
 
